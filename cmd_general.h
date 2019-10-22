62eb56f5a5ac556fa2506a9afcabf45864d0dfcf
upszhang <upszhang@163.com>
2019-10-22 16:23:04 +0800

    add cmd_general.h

---
+++ cmd_general.h
@@ -0,0 +1,17 @@
+#ifndef __CMD_GENERAL_H__
+#define __CMD_GENERAL_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "cmd_inc.h"
+#include "DSC_inc.h"
+
+#define PRODUCT_NOPPORO         (1)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
